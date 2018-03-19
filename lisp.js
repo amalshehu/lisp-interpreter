422a6822c7a9f7bc4a06e04eb4f1c7bbcc7b1e67
amalshehu <amalshehu@gmail.com>
2018-03-19 14:38:06 +0530

    Fix number sum and diff

--- lisp.js
+++ lisp.js
@@ -13,7 +13,39 @@
 // })
 
 let ENV = {}
+const expressionEvaluator = code => {
+  token = code.substr(0, code.indexOf(' '))
+  code = code.slice(1)
+  space(code) ? (code = space(code)[1]) : code
+  switch (token) {
+    case '+':
+      const sum = (a, b) => a + b
+      return [numberEvaluator(code)[0].reduce(sum), numberEvaluator(code)[1]]
+    case '-':
+      const sub = (a, b) => a - b
+      return [numberEvaluator(code)[0].reduce(sub), numberEvaluator(code)[1]]
+    case 'define':
+      return number(code)
+      break
+    default:
+      break
+  }
+}
 
+let numArray = []
+
+const numberEvaluator = code => {
+  token = code.substr(0, code.indexOf(' '))
+  if (!isFinite(token)) return null
+  if (code === ')') {
+    return [numArray, code]
+  }
+  parseNumbers = number(code)
+  numArray.push(parseNumbers[0])
+  code = parseNumbers[1]
+  space(code) ? (code = space(code)[1]) : code
+  return numberEvaluator(code)
+}
 const spaceRe = /^\s+|\s+$/
 const space = str => {
   let match
@@ -23,27 +55,26 @@ const space = str => {
     : null
 }
 
-export const expressionEvaluator = p => {
-  return code => {
-    if (code === undefined) return null
-    let fetched
-    for (let parser of p) {
-      fetched = parser(code)
-      if (fetched != null) {
-        return fetched
-      }
-    }
-    return null
+const numRe = /^[-+]?[0-9]*\.?[0-9]+([eE][-+]?[0-9]+)?/
+const number = str => {
+  let match = str.match(numRe)
+  if (match) {
+    return [parseFloat(match[0]), str.slice(match[0].length)]
   }
+  return null
 }
 
 const parse = program => {
-  parsed = []
+  let extracted
   if (program[0] != '(') return null
-  program.slice(1)
-  while (program[0] != ')') {
+  program = program.slice(1)
+  while (program != ')') {
     space(program) ? (program = space(program)[1]) : program
+    extracted = expressionEvaluator(program)
+    if (extracted) {
+      program = extracted[1]
+    }
   }
-  console.log('Parsed', parsed)
+  return [extracted[0], program.slice(1)]
 }
-parse('(define x 100))')
+console.log(parse('(- 1 2 3)'))

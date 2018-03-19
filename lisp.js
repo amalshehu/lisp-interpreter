aa63190cb708a296c7b149e690acf601a3722831
amalshehu <amalshehu@gmail.com>
2018-03-19 12:23:20 +0530

    Add space parser

--- lisp.js
+++ lisp.js
@@ -1,19 +1,36 @@
-let readline = require('readline')
+// let readline = require('readline')
 
-let reader = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-  terminal: true
-})
-reader.on('line', line => {
-  let tokens = tokenize(line)
-  console.log(tokens)
-  reader.close()
-})
+// let reader = readline.createInterface({
+//   input: process.stdin,
+//   output: process.stdout,
+//   terminal: true
+// })
+// reader.on('line', line => {
+//   let tokens = tokenize(line)
+//   parse(tokens)
 
-const tokenize = program =>
-  program
-    .replace(/[(]/g, ' ( ')
-    .replace(/[)]/g, ' ) ')
-    .split(' ')
-    .filter(Boolean)
+//   reader.close()
+// })
+
+let ENV = {}
+
+const spaceRe = /^\s+|\s+$/
+const space = str => {
+  let match
+  return (str && str.startsWith(' ')) || (str && str.startsWith('\n'))
+    ? ((match = str.match(spaceRe)),
+      match ? [match[0], str.replace(spaceRe, '')] : null)
+    : null
+}
+
+const parse = tokens => {
+  parsed = []
+  if (tokens[0] != '(') return null
+  tokens.shift()
+  while (tokens[0] != ')') {
+    parsed.push(tokens[0])
+    tokens.shift()
+  }
+  console.log('Parsed', parsed)
+}
+parse(tokenize('(define square (lambda(x)(x * x)))'))

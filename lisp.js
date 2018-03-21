6b6e8575f623ef9d10d80c7a28e8146030eda911
amalshehu <amalshehu@gmail.com>
2018-03-21 15:56:44 +0530

    Fix if parser  through valueParser

--- lisp.js
+++ lisp.js
@@ -15,35 +15,40 @@ const skipSpace = code => {
   return match ? [match[0], code.replace(/^\s+/, '')] : null
 }
 
-const number = code => {
+const extractNum = code => {
   let match = code.match(/^[-+]?[0-9]*\.?[0-9]+([eE][-+]?[0-9]+)?/)
   return match ? [parseFloat(match[0]), code.slice(match[0].length)] : null
 }
 
-const operator = code => {
+const extractOperator = code => {
   let match = code.match(/^(\+|-|\*|\/|=|>|<|>=|<=)/)
   return match ? [match[0], code.slice(match[0].length)] : null
 }
 
-const stringx = code => {
+const extractString = code => {
   let match
-  return code && code.startsWith('"')
-    ? ((match = code.match(/^"(?:\\"|.)*?"/)),
-      match && match[0] != undefined
-        ? [match[0].slice(1, -1), code.replace(match[0], '')]
-        : SyntaxError('Syntax Error'))
-    : null
+  return code && code.startsWith('"') ?
+    ((match = code.match(/^"(?:\\"|.)*?"/)),
+      match && match[0] != undefined ? [match[0].slice(1, -1), code.replace(match[0], '')] :
+      SyntaxError('Syntax Error')) :
+    null
 }
-const ifx = code => {
-  if (!code.slice(1).startsWith('if')) return null
-  else {
-    code = code.slice(3)
+const extractIf = code => {
+  if (!code.startsWith('if')) return null
+  code = code.slice(2)
+  let value = valueParser(code)
+  if (value == null) {
     skipSpace(code) ? (code = skipSpace(code)[1]) : code
-    let match = code.split(/(\(.+?\))/)
-    console.log(match)
-
-    // return valueParser(match[0]) ? valueParser(match[1]) : valueParser(match[2])
+    const ifRe = /(\(.+?\))\s*(\(.+?\))\s*(\(.+?\))/
+    let match = code.split(ifRe).filter(Boolean)
+    return [
+      valueParser(match[0])[0] ?
+      valueParser(match[1])[[0]] :
+      valueParser(match[2])[0],
+      match[3]
+    ]
   }
+  return [value[0], value[1]]
 }
 const nativeFunctions = {
   '+': (a, b) => a + b,
@@ -59,7 +64,7 @@ const nativeFunctions = {
 }
 
 const parseExpr = code => {
-  if (code != undefined && !code.startsWith('(')) {
+  if (!code.startsWith('(')) {
     return null
   }
   let box = []
@@ -71,7 +76,7 @@ const parseExpr = code => {
       box.push(factoryOut[0])
       code = factoryOut[1]
     }
-    if (!code.includes(')')) throw Error('Expected closing.')
+    // if (!code.includes(')')) throw Error('Expected closing.')
     if (code.startsWith(')')) {
       return [evaluateExpr(box), code.slice(1)]
     }
@@ -81,11 +86,14 @@ const parseExpr = code => {
 
 const evaluateExpr = code => {
   const key = code[0]
-  code = code.slice(1)
-  if (code.length == 1 && key == '-') {
-    return parseFloat(`${key}${code[0]}`)
+  if (key && code.length > 1) {
+    code = code.slice(1)
+    if (code.length == 1 && key == '-') {
+      return parseFloat(`${key}${code[0]}`)
+    }
+    return code.reduce(nativeFunctions[key])
   }
-  return code.reduce(nativeFunctions[key])
+  return code
 }
 const factory = parsers => {
   return text => {
@@ -104,7 +112,12 @@ const factory = parsers => {
     return null
   }
 }
-const valueParser = factory([stringx, number, operator, parseExpr])
-const splValueParser = factory([ifx])
+const valueParser = factory([
+  parseExpr,
+  extractString,
+  extractNum,
+  extractOperator,
+  extractIf
+])
 
-console.log(ifx('(if (> 10 20) (+ 1 1) (+ 3 3))'))
+console.log(valueParser('(if (< 10 5) (+ 1 1) (+ 3 3))'))

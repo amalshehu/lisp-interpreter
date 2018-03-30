7d2a272a86ec810c5e62b9054cd9d3b6ece6243d
amalshehu <amalshehu@gmail.com>
2018-03-30 12:46:09 +0530

    Modify define function

--- lisp.js
+++ lisp.js
@@ -101,15 +101,15 @@ const extractDefine = code => {
   if (!code.startsWith('define')) return null
   code = code.slice(6)
   skipSpace(code) ? (code = skipSpace(code)[1]) : code
-  const symbol = code.slice(0, code.indexOf(' '))
-  code = code.slice(symbol.length)
+  const symbol = extractSymbol(code)
+  code = symbol[1]
   skipSpace(code) ? (code = skipSpace(code)[1]) : code
   let value = valueParser(code)
   if (value) {
-    ENV[symbol] = { value: value[0], type: typeof value[0] }
+    ENV[symbol[0]] = { value: value[0], type: typeof value[0] }
     // console.log()
   }
-  return [`value ${value[0]} assigned to ${symbol}`, value[1]]
+  return [`value ${value[0]} assigned to ${symbol[0]}`, value[1]]
 }
 
 const extractIf = code => {
